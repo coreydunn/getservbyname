666d938b5e0c67ebe475a691038d489a4ac08904
coreydunn <jumpingkanga@outlook.com>
2022-04-15 08:21:59 -0500

    UDP, TCP switches

--- main.c
+++ main.c
@@ -8,83 +8,116 @@
 #include"str.h"
 #include"vec.h"
 
+#define HELPSTR "usage: getservbyname -htuv [SERVICE|PORT]"
+
 int main(int argc,char**argv)
 {
-	struct servent*serv=NULL;
-	char*name=NULL;
+	Vec v=vec_new();
 	bool reverse=false;
 	bool verbose=false;
-	Vec v=vec_new();
+	bool showhelp=false;
+	char*name=NULL;
+	char*protocol="tcp";
+	struct servent*serv=NULL;
 
 	// Process arguments
 	for(int i=1;i<argc;++i)
 	{
-		if(strcmp(argv[i],"-v")==0)
+
+		// Short options
+		if(argv[i][0]=='-')
 		{
-			verbose=true;
-			continue;
+			for(size_t j=1;argv[i][j];++j)
+			{
+				switch(argv[i][j])
+				{
+
+					case 'h':
+						showhelp=true;
+						break;
+
+					case 't':
+						protocol="tcp";
+						break;
+
+					case 'u':
+						protocol="udp";
+						break;
+
+					case 'v':
+						verbose=true;
+						break;
+
+					default:
+					fprintf(stderr,"error: unrecognized option '%c'\n",argv[i][j]);
+						break;
+
+				}
+			}
 		}
 
-		vec_push(&v,argv[i]);
+		// Default: treat as service name (or port number)
+		else
+			vec_push(&v,argv[i]);
 	}
 
-	if(argc<2||!v.n)
-	{
-		fprintf(stderr,"error: service name not specified\n");
-		exit(1);
-	}
+	if(argc<2||showhelp)
+		printf("%s\n",HELPSTR);
 
 	// Process service names
-	for(size_t i=0;i<v.n;++i)
+	if(!showhelp)
 	{
-		name=v.b[i].b;
-
-		if(!name||!*name)
+		for(size_t i=0;i<v.n;++i)
 		{
-			fprintf(stderr,"error: name cannot be empty/null\n");
-			continue;
-		}
+			name=v.b[i].b;
 
-		serv=getservbyname(name,"tcp");
-
-		// Lookup by name failed -- attempt reverse lookup
-		if(!serv)
-		{
-			serv=getservbyport(ntohs(atoi(name)),"tcp");
-			if(serv)
-				reverse=true;
-		}
-
-		// Failed to find service
-		if(!serv)
-		{
-			fprintf(stderr,"error: cannot identify service '%s'\n",name);
-			continue;
-		}
-
-		// Output full info
-		if(verbose)
-		{
-			char**p=serv->s_aliases;
-
-			printf("%s %d %s",serv->s_name,serv->s_port,serv->s_proto);
-			printf(" [");
-			for(int i=0;p[i];++i)
+			if(!name||!*name)
 			{
-				printf("%s ",serv->s_aliases[i]);
-				if(p[i+1])
-					printf(", ");
+				fprintf(stderr,"error: name cannot be empty/null\n");
+				continue;
 			}
-			printf("]\n");
-		}
 
-		// Output basic info
-		else
-		{
-			if(reverse)
-				printf("%s %s\n",name,serv->s_name);
+			serv=getservbyname(name,protocol);
+
+			// Lookup by name failed -- attempt reverse lookup
+			if(!serv)
+			{
+				serv=getservbyport(ntohs(atoi(name)),protocol);
+				if(serv)
+					reverse=true;
+			}
+
+			// Failed to find service
+			if(!serv)
+			{
+				fprintf(stderr,"error: cannot identify service '%s'\n",name);
+				continue;
+			}
+
+			// Output full info
+			if(verbose)
+			{
+				char**p=serv->s_aliases;
+
+				printf("%s %d %s",serv->s_name,serv->s_port,serv->s_proto);
+				printf(" [");
+				for(int i=0;p[i];++i)
+				{
+					printf("%s ",serv->s_aliases[i]);
+					if(p[i+1])
+						printf(", ");
+				}
+				printf("]\n");
+			}
+
+			// Output basic info
 			else
-				printf("%s %d\n",name,htons(serv->s_port));
+			{
+				if(reverse)
+					printf("%s %s\n",name,serv->s_name);
+				else
+					printf("%s %d\n",name,htons(serv->s_port));
+			}
 		}
 	}
 
